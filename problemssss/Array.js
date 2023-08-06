f194e2d3b4f8a103e38afdd76d28c63947ce2caa
Kiranism <iamkiran2831@gmail.com>
2023-08-07 02:47:26 +0530

    udpates

--- problemssss/Array.js
+++ problemssss/Array.js
@@ -200,3 +200,69 @@ const modArr = (arr) => {
 };
 
 console.log(modArr([1, 2]));
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0, 0, 9, 0, 0];
+
+// function productExceptSelf(nums) {
+//   const n = nums.length;
+//   const ans = new Array(n);
+
+//   for (let i = 0; i < n; i++) {
+//     let pro = 1;
+//     for (let j = 0; j < n; j++) {
+//       if (i === j) continue;
+//       pro *= nums[j];
+//     }
+//     ans[i] = pro === 0 ? 0 : pro;
+//   }
+
+//   return ans;
+// }
+
+
+function productExceptSelf(nums) {
+  const n = nums.length;
+  const ans = new Array(n);
+
+
+}
+console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
+console.log(productExceptSelf([0, 0]));
+
+console.log(5 / 2, 5 % 2);
+
+
+var productExceptSelf = function(nums) {
+  const length = nums.length;
+  const result = new Array(length).fill(1);
+
+  // Calculate the product of elements before the current index
+  let productBefore = 1;
+  for (let i = 0; i < length; i++) {
+    result[i] *= productBefore;
+    productBefore *= nums[i];
+  }
+
+  // Calculate the product of elements after the current index
+  let productAfter = 1;
+  for (let i = length - 1; i >= 0; i--) {
+    result[i] *= productAfter;
+    productAfter *= nums[i];
+  }
+
+  return result;
+};
