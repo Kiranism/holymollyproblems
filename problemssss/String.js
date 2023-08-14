54e2c180361988671bb0dc389aaef483822fa94e
Kiranism <iamkiran2831@gmail.com>
2023-08-15 01:10:27 +0530

    updates

--- problemssss/String.js
+++ problemssss/String.js
@@ -402,3 +402,48 @@ var firstUniqChar = function (s) {
 };
 
 console.log("unique", firstUniqChar("loveleetcode"));
+
+/* 171. Excel Sheet Column Number
+Easy
+4.4K
+341
+Companies
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701 */
+
+var titleToNumber = function (columnTitle) {
+  let result = 0;
+  let length = columnTitle.length;
+  for (let i = 0; i < length; i++) {
+    result += (columnTitle.charCodeAt(i) - 64) * Math.pow(26, length - (i + 1));
+  }
+  return result;
+};
+console.log("titleToNum", titleToNumber("AB"));
+
+let char = "titleToNum";
+console.log("tit", Math.pow(2, 2));
