0327fa006fcbcee3547e4e8ad2992a14362e4186
Kiranism <iamkiran2831@gmail.com>
2023-08-31 21:31:23 +0530

    clown

--- problemssss/Array.js
+++ problemssss/Array.js
@@ -657,7 +657,6 @@ var pivot = function (nums) {
 };
 console.log("pivot", pivot([1, 4, 2, 5, 0]));
 
-
 var majorityElement = function (nums) {
   let obj = {};
 
@@ -667,3 +666,36 @@ var majorityElement = function (nums) {
     if (obj[n] > nums.length / 2) return n;
   }
 };
+
+/* Example 1:
+
+26. Remove Duplicates from Sorted Array
+Easy
+
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores). */
+
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    const n = nums[j];
+    if (nums[i] !== nums[j]) {
+      nums[i] = n;
+      i++;
+    }
+  }
+  return i + 1;
+};
+
+console.log("removeDup", removeDuplicates([1, 1, 2]));
+
+// clown push.
