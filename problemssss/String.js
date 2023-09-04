45aa3ca449498267a9472a138bb8adae4fff2581
Kiranism <iamkiran2831@gmail.com>
2023-09-05 01:58:43 +0530

    concat

--- problemssss/String.js
+++ problemssss/String.js
@@ -590,3 +590,32 @@ function checkOccur(arr, num) {
 }
 
 console.log("check", checkOccur([1, 1, 2, 2, 2, 2, 3], 2));
+
+/* 796. Rotate String
+Easy
+3.2K
+124
+Companies
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+  */
+
+var rotateString = function (s, goal) {
+  return s.concat(s).includes(goal);
+};
+
+console.log("rotateString", rotateString("abcde", "cdeab"));
+console.log("rotateString", rotateString("abcde", "abced"));
